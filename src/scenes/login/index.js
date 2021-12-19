ae2656866857223e91df9b9b629a2c1701301645
Kevin Sandoval <kevinsandoval.co@gmail.com>
2021-12-19 15:43:06 -0700

    Support Native Biometric Authentication

--- src/scenes/login/index.js
+++ src/scenes/login/index.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {View, Text, Button} from 'react-native';
 import auth from '@react-native-firebase/auth';
+import ReactNativeBiometrics from 'react-native-biometrics';
 
 export default function LoginScreen({navigation}) {
   // Set an initializing state whilst Firebase connects
@@ -20,6 +21,33 @@ export default function LoginScreen({navigation}) {
 
   if (initializing) return null;
 
+  const handleLogin = () => {
+    ReactNativeBiometrics.isSensorAvailable().then(
+      ({available, biometryType, error}) => {
+        if (available) {
+          switch (biometryType) {
+            case ReactNativeBiometrics.TouchID:
+            case ReactNativeBiometrics.FaceID:
+            case ReactNativeBiometrics.Biometrics:
+              ReactNativeBiometrics.simplePrompt({
+                promptMessage: 'Verify your identity',
+              }).then(({success}) => {
+                if (success) {
+                  console.log('successful biometrics provided\n');
+                  console.log('<<<DEV SIGN IN NOW>>>');
+                } else {
+                  console.log('user cancelled biometric prompt');
+                }
+              });
+              break;
+            default:
+              console.error(`Biometrics type not supported: ${biometryType}`);
+          }
+        } else console.log(error);
+      },
+    );
+  };
+
   if (!user) {
     return (
       <View>
@@ -30,6 +58,7 @@ export default function LoginScreen({navigation}) {
             navigation.navigate('Home');
           }}
         />
+        <Button title="Log In" onPress={handleLogin} />
       </View>
     );
   }
