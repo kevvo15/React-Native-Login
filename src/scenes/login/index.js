7b47e8a887d20fbae8eaad6cbf220b996f3bfeb3
Kevin Sandoval <kevinsandoval.co@gmail.com>
2021-12-19 17:16:02 -0700

    Installed Formik dependencies to enable user email/pw sign in

--- src/scenes/login/index.js
+++ src/scenes/login/index.js
@@ -1,17 +1,28 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, Button} from 'react-native';
+import {
+  ActivityIndicator,
+  Alert,
+  Button,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import {Formik} from 'formik';
 import auth from '@react-native-firebase/auth';
 import ReactNativeBiometrics from 'react-native-biometrics';
 
 export default function LoginScreen({navigation}) {
-  // Set an initializing state whilst Firebase connects
-  const [initializing, setInitializing] = useState(true);
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   // Handle user state changes
   function onAuthStateChanged(user) {
     setUser(user);
-    if (initializing) setInitializing(false);
+    if (user) {
+      setLoading(false);
+      navigation.replace('Home');
+      console.log('User signed in successfully!');
+    }
   }
 
   useEffect(() => {
@@ -19,9 +30,18 @@ export default function LoginScreen({navigation}) {
     return subscriber; // unsubscribe on unmount
   }, []);
 
-  if (initializing) return null;
+  if (loading)
+    return (
+      <View>
+        <ActivityIndicator
+          size="large"
+          color="black"
+          style={{flex: 1, justifyContent: 'center'}}
+        />
+      </View>
+    );
 
-  const handleLogin = () => {
+  const biometricLogin = () => {
     ReactNativeBiometrics.isSensorAvailable().then(
       ({available, biometryType, error}) => {
         if (available) {
@@ -48,17 +68,78 @@ export default function LoginScreen({navigation}) {
     );
   };
 
+  const formLogin = (email, password) => {
+    setLoading(true);
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(error => {
+        setLoading(false);
+        switch (error.code) {
+          case 'auth/invalid-email':
+            Alert.alert('Invalid Email Format Submitted');
+            break;
+          case 'auth/wrong-password':
+            Alert.alert('Incorrect Email or Password Submitted');
+            break;
+          case 'auth/user-not-found':
+            Alert.alert('Incorrect Email or Password Submitted');
+            break;
+        }
+      });
+  };
+
   if (!user) {
     return (
       <View>
-        <Text>Login</Text>
-        <Button
-          title="Go to Home"
-          onPress={() => {
-            navigation.navigate('Home');
-          }}
-        />
-        <Button title="Log In" onPress={handleLogin} />
+        <Formik
+          initialValues={{email: '', password: ''}}
+          // validationSchema={loginSchema}
+          onSubmit={(values, {resetForm}) => {
+            resetForm({
+              values: {
+                email: '',
+                password: '',
+              },
+            });
+            formLogin(values.email, values.password);
+            console.log('Email: ' + values.email + ' PW: ' + values.password);
+          }}>
+          {formikprops => (
+            <>
+              <View>
+                <TextInput
+                  // style={globalStyles.input}
+                  placeholder="Email                            "
+                  onChangeText={formikprops.handleChange('email')}
+                  value={formikprops.values.email}
+                />
+                <TextInput
+                  // style={globalStyles.input}
+                  placeholder="Password                         "
+                  secureTextEntry={true}
+                  onChangeText={formikprops.handleChange('password')}
+                  value={formikprops.values.password}
+                />
+                <Button
+                  title="Log In"
+                  color="black"
+                  onPress={formikprops.handleSubmit}
+                />
+              </View>
+              <View>
+                <Text>New User?</Text>
+                <Button
+                  title="Register Here"
+                  color="black"
+                  onPress={() => {
+                    //navigation.replace('Signup');
+                    console.log('Register Button Clicked');
+                  }}
+                />
+              </View>
+            </>
+          )}
+        </Formik>
       </View>
     );
   }
@@ -66,6 +147,7 @@ export default function LoginScreen({navigation}) {
   return (
     <View>
       <Text>Welcome {user.email}</Text>
+      <Button title="Verify ID to Continue" onPress={biometricLogin} />
     </View>
   );
 }
